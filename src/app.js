e0470e81207ca7d4b0ee40ab9219ede80b18f70d
Jonathan <oldschoolspaceempire@gmail.com>
2022-03-28 10:51:28 -0500

    made styled-component, also made compound component

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import jumboData from './fixtures/jumbo'
+import Jumbotron from './components/jumbotron';
 
 export default function App() {
-  return (
-    <p>Hello</p>
+  return (<div>
+        <Jumbotron.Container>
+          {jumboData.map((item) => (
+            <Jumbotron key={item.id} direction={item.direction}>
+            <p>hello</p>
+            </Jumbotron>
+          ))}
+        </Jumbotron.Container>
+    </div>
   );
 }
 
